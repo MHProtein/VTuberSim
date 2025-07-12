89443896c70c5ca1d53154117ff9c7beb9fa2d56
MHProtein <a1379191885@gmail.com>
2025-07-12 20:44:05 +0800

    Fixed skip turn button behaviour

---
+++ Assets/Scripts/VTuber/BattleSystem/UI/VSkipTurnButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.UI;
+using VTuber.Core.EventCenter;
+using VTuber.Core.Foundation;
+
+namespace VTuber.BattleSystem.UI
+{
+    public class VSkipTurnButton : VUIBehaviour
+    {
+        private Button button;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            button = GetComponent<Button>();
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            VBattleRootEventCenter.Instance.RegisterListener(VRootEventKey.OnTurnChange,
+                dict => button.interactable = true);
+            VBattleRootEventCenter.Instance.RegisterListener(VRootEventKey.OnTurnEnd,
+                dict => button.interactable = false);
+        }
+    }
+}
