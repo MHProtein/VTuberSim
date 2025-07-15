c11c8e964601738655bb3c5219b9b7772633009d
AntiiRain <2814571412@qq.com>
2025-07-16 00:44:21 +0800

    UI initial update

---
+++ Assets/Scripts/VTuber/ScheduleSystem/UI/UIEventButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using VTuber.ScheduleSystem.Event;
+
+public class UIEventButton : MonoBehaviour
+{
+    public Text nameText;
+    public Button button;
+
+    private ScheduleEventConfiguration _config;
+
+    public void Setup(ScheduleEventConfiguration config, System.Action<ScheduleEventConfiguration> onClick)
+    {
+        _config = config;
+        nameText.text = config.eventName;
+
+        button.onClick.AddListener(() => onClick?.Invoke(_config));
+    }
+}
