a341cecbc48c52bb2a34bbbd01a09b7530658230
MHProtein <a1379191885@gmail.com>
2025-07-29 17:07:09 +0800

    fixed buff percentage buff cannot add negative value

--- Assets/Scripts/VTuber/ScheduleSystem/UI/ScheduleCreator/VScheduleCreator.cs
+++ Assets/Scripts/VTuber/ScheduleSystem/UI/ScheduleCreator/VScheduleCreator.cs
@@ -1,58 +1,58 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-namespace VTuber.ScheduleSystem.UI
-{
-    public class VScheduleCreator : VScheduleTable
-    {
-        public Vector2Int slotSize;
-        [SerializeField] protected GameObject itemPrefab;
-        
-        protected VScheduleCreatorSlot[,] slots;
-        protected List<EventData> items;
-        
-        [SerializeField] private List<EventData> eventDatas;
-
-        protected override void Awake()
-        {
-            slots = new VScheduleCreatorSlot[slotSize.y, slotSize.x];
-            var slotList = GetComponentsInChildren<VScheduleCreatorSlot>();
-            
-            int i = 0; 
-            for (int y = 0; y < slotSize.y; y++)
-            {
-                for (int x = 0; x < slotSize.x; x++)
-                {    
-                    slots[y, x] = slotList[i++];
-                }
-            }
-        }
-        
-        VScheduleCreatorSlot GetAvailableSlot()
-        {
-            for (int x = 0; x < slotSize.x; x++)
-            {
-                for (int y = 0; y < slotSize.y; y++)
-                {
-                    if (slots[y, x].Item is null)
-                        return slots[y, x];
-                }
-            }
-
-            return null;
-        }
-        
-        protected override void Start()
-        {
-            base.Start();
-            foreach (var eventData in eventDatas)
-            {
-                var slot = GetAvailableSlot();
-                var eventObj = Instantiate(itemPrefab, slot.transform);
-                var eventUI = eventObj.GetComponent<VEventDataUI>();
-                eventUI.Initialize(eventData);
-                slot.SetItem(eventUI);
-            }
-        }
-    }
-}
+﻿// using System.Collections.Generic;
+// using UnityEngine;
+//
+// namespace VTuber.ScheduleSystem.UI
+// {
+//     public class VScheduleCreator : VScheduleTable
+//     {
+//         public Vector2Int slotSize;
+//         [SerializeField] protected GameObject itemPrefab;
+//         
+//         // protected VScheduleCreatorSlot[,] slots;
+//         protected List<EventData> items;
+//         
+//         [SerializeField] private List<EventData> eventDatas;
+//
+//         protected override void Awake()
+//         {
+//             slots = new VScheduleCreatorSlot[slotSize.y, slotSize.x];
+//             var slotList = GetComponentsInChildren<VScheduleCreatorSlot>();
+//             
+//             int i = 0; 
+//             for (int y = 0; y < slotSize.y; y++)
+//             {
+//                 for (int x = 0; x < slotSize.x; x++)
+//                 {    
+//                     slots[y, x] = slotList[i++];
+//                 }
+//             }
+//         }
+//         
+//         VScheduleCreatorSlot GetAvailableSlot()
+//         {
+//             for (int x = 0; x < slotSize.x; x++)
+//             {
+//                 for (int y = 0; y < slotSize.y; y++)
+//                 {
+//                     if (slots[y, x].Item is null)
+//                         return slots[y, x];
+//                 }
+//             }
+//
+//             return null;
+//         }
+//         
+//         protected override void Start()
+//         {
+//             base.Start();
+//             foreach (var eventData in eventDatas)
+//             {
+//                 var slot = GetAvailableSlot();
+//                 var eventObj = Instantiate(itemPrefab, slot.transform);
+//                 var eventUI = eventObj.GetComponent<VEventDataUI>();
+//                 eventUI.Initialize(eventData);
+//                 slot.SetItem(eventUI);
+//             }
+//         }
+//     }
+// }
