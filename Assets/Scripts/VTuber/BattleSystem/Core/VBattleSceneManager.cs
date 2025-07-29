391e284b0e0f1d0d305ca2420237251a78710887
MHProtein <a1379191885@gmail.com>
2025-07-29 19:09:04 +0800

    created battle scene manager

---
+++ Assets/Scripts/VTuber/BattleSystem/Core/VBattleSceneManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using VTuber.BattleSystem.Card;
+using VTuber.Character;
+using VTuber.Core.Foundation;
+
+namespace VTuber.BattleSystem.Core
+{
+    public class VBattleSceneManager : VMonoBehaviour
+
+    {
+        [SerializeField] private GameObject battleRoot;
+        [SerializeField] private VBattle _battle;
+        [SerializeField] private VCharacterConfiguration _characterConfiguration;
+        private VCharacter character;
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            VBattleResourcesLoader loader = new VBattleResourcesLoader(@"Assets\Resources\Configurations\NewCards.xlsx");
+            character = new VCharacter(_characterConfiguration);
+            var cardConfigs = loader.Load();
+            List<VCard> cards = new List<VCard>();
+
+            foreach (var cardConfig in cardConfigs)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    var card = cardConfig.CreateCard();
+                    if(card is not null)
+                        cards.Add(card);
+                }
+            }
+            character.CardLibrary.AddCards(cards);
+            InitializeBattle();
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+        }
+
+        public void InitializeBattle()
+        {
+            //battleRoot.SetActive(true);
+            _battle.InitializeBattle(character.AttributeManager, character.CardLibrary, 10);
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+    }
+}
