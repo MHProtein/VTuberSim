12f3c5436718a13f7328ffb489b7dd6ae405bc2c
MHProtein <a1379191885@gmail.com>
2025-07-29 19:13:34 +0800

    Update VScheduleCreatorSlot.cs

--- Assets/Scripts/VTuber/ScheduleSystem/UI/ScheduleCreator/VScheduleCreatorSlot.cs
+++ Assets/Scripts/VTuber/ScheduleSystem/UI/ScheduleCreator/VScheduleCreatorSlot.cs
@@ -1,21 +1,21 @@
-﻿// using UnityEngine.EventSystems;
-// using VTuber.Core.Foundation;
-//
-// namespace VTuber.ScheduleSystem.UI
-// {
-//     public class VScheduleCreatorSlot : VUIBehaviour
-//     {
-//         public VEventDataUI Item => _item;
-//         private VEventDataUI _item;
-//         
-//         public void SetItem(VEventDataUI item)
-//         {
-//             _item = item;
-//         }
-//
-//         public void RemoveItem()
-//         {
-//             _item = null;
-//         }
-//     }
-// }
+﻿using UnityEngine.EventSystems;
+using VTuber.Core.Foundation;
+
+namespace VTuber.ScheduleSystem.UI
+{
+    public class VScheduleCreatorSlot : VUIBehaviour
+    {
+        public VEventDataUI Item => _item;
+        private VEventDataUI _item;
+        
+        public void SetItem(VEventDataUI item)
+        {
+            _item = item;
+        }
+
+        public void RemoveItem()
+        {
+            _item = null;
+        }
+    }
+}
