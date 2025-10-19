403bc99167fde74db0932950b1d079457fb69dcb
AntiiRain <2814571412@qq.com>
2025-10-18 23:39:38 -0400

    Relic scripts update

--- Assets/Scripts/VTuber/EventSystem/UI/VPhaseEndingSelectionMenu.cs
+++ Assets/Scripts/VTuber/EventSystem/UI/VPhaseEndingSelectionMenu.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using VTuber.BattleSystem.Core;
 using VTuber.Core.EventCenter;
 using VTuber.Core.Foundation;
+using VTuber.Core.Managers;
 using VTuber.Core.SE;
 using VTuber.Dialogue.UI;
 using VTuber.ScheduleSystem.Events;
+using VTuber.ScheduleSystem.Events.DialogueEvent;
+using VTuber.Relic;
+// Make sure you have the correct using statements for your project's resource/game managers
+// using VTuber.Core.Data; // Example
+// using VTuber.Relics; // Example
 
 namespace VTuber.EventSystem.UI
 {
@@ -13,9 +20,19 @@ namespace VTuber.EventSystem.UI
     {
         private List<VPhaseEndingOption> _options;
         private VPhaseEndingOption _selectedOption;
-        
-        [SerializeField] private Button confirmButton;
+
+        [Header("Original References")] [SerializeField]
+        private Button confirmButton;
+
         [SerializeField] private Transform grid;
+
+        // --- NEW CODE START ---
+        [Header("Relic Display")] [SerializeField]
+        private GameObject relicUiPrefab; // Drag your Relic UI Prefab here in the Inspector
+
+        [SerializeField] private Transform characterRelicsContainer; // Drag the container you created
+        [SerializeField] private Transform inheritableRelicsContainer; // Drag the other container
+        // --- NEW CODE END ---
 
 
         public void Initialize(GameObject endingPrefab, List<VStreamEvent> endings)
@@ -29,15 +46,26 @@ namespace VTuber.EventSystem.UI
                 option.Initialize(ending, this);
                 _options.Add(option);
             }
+
+            // --- NEW CODE START ---
+            // Ensure relic displays are empty when the menu first opens
+            ClearRelicDisplays();
+            // --- NEW CODE END ---
         }
-        
+
         public void SelectOption(VPhaseEndingOption option)
         {
             VAudioPlayer.Instance.PlayStaticSFX(VSFXType.Selection);
             confirmButton.interactable = true;
-            if(_selectedOption is not null)
+            if (_selectedOption is not null)
                 _selectedOption.Unselect();
             _selectedOption = option;
+
+            // --- NEW CODE START ---
+            // When a new option is selected, clear the old relics and show the new ones
+            ClearRelicDisplays();
+            DisplayRelicsForOption(option);
+            // --- NEW CODE END ---
         }
 
         public void Confirm()
@@ -53,10 +81,82 @@ namespace VTuber.EventSystem.UI
             {
                 Destroy(option.gameObject);
             }
+
             _options.Clear();
             _selectedOption = null;
             VEventSystemUI.Instance.ClosePhaseEndingSelectionMenu();
         }
-        
+
+        // --- NEW METHODS START ---
+
+        /// <summary>
+        /// Clears all instantiated relic icons from the display containers.
+        /// </summary>
+        private void ClearRelicDisplays()
+        {
+            foreach (Transform child in characterRelicsContainer)
+            {
+                Destroy(child.gameObject);
+            }
+
+            foreach (Transform child in inheritableRelicsContainer)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        /// <summary>
+        /// Fetches and displays the relics associated with the selected event option.
+        /// </summary>
+        private void DisplayRelicsForOption(VPhaseEndingOption option)
+        {
+            if (relicUiPrefab == null) return;
+
+            ClearRelicDisplays();
+            VStreamEvent streamEvent = option.E;
+
+            // 1. Get the Dialog object
+            Dialog dialog = VResourcesManager.Instance.TryGetDialog(streamEvent.dialogueNode);
+
+            if (dialog != null)
+            {
+                // 2. Get the list of relic configurations
+                List<VRelicConfiguration> relicConfigs = dialog.GetRelics();
+
+                if (relicConfigs != null)
+                {
+                    foreach (var config in relicConfigs)
+                    {
+                        // 3. Create the VRelic directly from the configuration object
+                        VRelic relicData = config.CreateRelic(); // <-- THE CORRECTED LINE
+
+                        if (relicData != null)
+                        {
+                            GameObject relicGo = Instantiate(relicUiPrefab, characterRelicsContainer);
+                            relicGo.GetComponent<VRelicUI>().Initialize(relicData);
+                        }
+                    }
+                }
+            }
+            // 2. Display inheritable relics (This part was likely already correct)
+            var inheritableRelicReward =
+                VGameManager.Instance.ReincarnationConfiguration.relicRewards.Find(x =>
+                    streamEvent.EventID == x.eventID);
+            if (inheritableRelicReward != null)
+            {
+                foreach (var relicId in inheritableRelicReward.relicIDs)
+                {
+                    // As before, you'll need a manager to get relic data from an ID.
+                    VRelic relicData =
+                        VDataManager.Instance.CreateRelicByID(relicId); // This is an assumption, but a common pattern.
+
+                    if (relicData != null)
+                    {
+                        GameObject relicGo = Instantiate(relicUiPrefab, inheritableRelicsContainer);
+                        relicGo.GetComponent<VRelicUI>().Initialize(relicData);
+                    }
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/VTuber/EventSystem/UI/VRelicUI.cs
+++ Assets/Scripts/VTuber/EventSystem/UI/VRelicUI.cs
@@ -1,19 +1,21 @@
-﻿
-using System.Collections;
-using System.Collections.Generic;
+﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using VTuber.Relic; // Add this using statement
+// using TMPro; 
 
-public class VRelicUI : MonoBehaviour
+public class VRelicUI : MonoBehaviour 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Image relicIcon;
+    [SerializeField] private TextMeshProUGUI relicNameText;
 
-    // Update is called once per frame
-    void Update()
+    // The parameter is now the correct type
+    public void Initialize(VRelic relicData) // <-- Use the base class here
     {
-        
+        if (relicData != null)
+        {
+            relicIcon.sprite = relicData.Icon;
+            relicNameText.text = relicData.GetRelicName();
+        }
     }
 }
